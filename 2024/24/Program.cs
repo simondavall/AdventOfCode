d5bf0a0d9e4f45fc3f2253cc54b40c878e7e4640
Simon Da Vall <simon.davall@gmail.com>
2025-01-04 23:17:04 +0000

    Aoc2024 Dayt24

---
+++ 2024/24/Program.cs
@@ -0,0 +1,213 @@
+﻿using System.Diagnostics;
+
+namespace _24;
+
+internal static class Program
+{
+    private static readonly Dictionary<string, int> Wires = [];
+    private static Dictionary<string, (string input1, string input2, string gate)> Connections = [];
+    
+    internal static void Main()
+    {
+        const string aocDay = "24";
+        const string aocYear = "2024";
+        const string path = $"/home/sdv/Documents/Projects/Aoc/{aocYear}/{aocDay}/";
+
+        const string filename = "input.txt";
+        var inputs = File.ReadAllText($"{path}{filename}").Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+        
+        foreach (var wire in inputs[0].Split('\n', StringSplitOptions.RemoveEmptyEntries))
+        {
+            var items = wire.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+            Wires.Add(items[0], int.Parse(items[1]));
+        }
+        
+        var connArray = inputs[1].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        Connections = connArray
+            .Select(connection => connection.Split(' '))
+            .ToDictionary(output => output[4], formula => (formula[0], formula[2], formula[1]));
+
+        Console.WriteLine($"Part 1: {PartOne()}");
+        Console.WriteLine($"Part 2: {PartTwo()}");
+    }
+
+    private static long PartOne()
+    {
+        var zWires = new List<int>();
+        var i = 0;
+        
+        while (true)
+        {
+            var key = Wire('z', i);
+            if (!Connections.ContainsKey(key))
+                break;
+            zWires.Add(GetOutput(key));
+            i++;
+        }
+
+        zWires.Reverse();
+        return Convert.ToInt64(string.Join("", zWires), 2);
+    }
+    
+    private static long PartTwo()
+    {
+        List<string> swappedWires = [];
+
+        var repeat = 0;
+        while (repeat++ < 4)
+        {
+            var foundSwap = false;
+            var errorLine = FindError();
+            foreach (var x in Connections.Keys)
+            {
+                foreach (var y in Connections.Keys)
+                {
+                    if (x == y)
+                        continue;
+                    (Connections[x], Connections[y]) = (Connections[y], Connections[x]);
+                    if (FindError() > errorLine)
+                    {
+                        swappedWires.Add(x);
+                        swappedWires.Add(y);
+                        foundSwap = true;
+                        break;
+                    }
+                    (Connections[x], Connections[y]) = (Connections[y], Connections[x]);
+                }
+
+                if (foundSwap)
+                    break;
+            }
+        }
+        
+        swappedWires.Sort();
+        Console.WriteLine(string.Join(",", swappedWires));
+            
+        return 0;
+    }
+
+    private static int GetOutput(string wire)
+    {
+        if (Wires.TryGetValue(wire, out var value))
+            return value;
+
+        var (input1, input2, gate) = Connections[wire];
+        Wires[wire] = GetOutput(gate, GetOutput(input1), GetOutput(input2));
+
+        return Wires[wire];
+    }
+
+    private static int GetOutput(string gate, int input1, int input2)
+    {
+        var output = gate switch
+        {
+            "AND" => input1 & input2,
+            "OR" => input1 | input2,
+            "XOR" => input1 ^ input2,
+            _ => throw new UnreachableException($"Oops, unknown gate {gate}")
+        };
+        
+        return output;
+    }
+    
+    private static int FindError()
+    {
+        var i = 0;
+
+        while (true)
+        {
+            if (!CheckGraph(i))
+                break;
+            i++;
+        }
+
+        return i;
+    }
+    
+    private static bool CheckGraph(int n)
+    {
+        return CheckZWires(Wire('z', n), n);
+    }
+    
+    private static bool CheckZWires(string wire, int n)
+    {
+        if (!Connections.TryGetValue(wire, out var value))
+            return false;
+
+        var (input1, input2, gate) = value;
+        if (gate != "XOR")
+            return false;
+        if (n == 0)
+            return SortedPair(input1, input2) == ("x00", "y00");
+
+        return (CheckXor(input1, n) && CheckCarryBit(input2, n)) 
+               || (CheckXor(input2, n) && CheckCarryBit(input1, n));
+    }
+    
+    private static bool CheckXor(string wire, int n)
+    {
+        if (!Connections.TryGetValue(wire, out var value))
+            return false;
+        
+        var (input1, input2, gate) = value;
+        if (gate != "XOR")
+            return false;
+
+        return SortedPair(input1, input2) == (Wire('x', n), Wire('y', n));
+    }
+
+    private static bool CheckCarryBit(string wire, int n)
+    {
+        if (!Connections.TryGetValue(wire, out var value))
+            return false;
+        
+        var (input1, input2, gate) = value;
+        if (n == 1)
+        {
+            if (gate != "AND")
+                return false;
+            return SortedPair(input1, input2) == ("x00", "y00");
+        }
+
+        if (gate != "OR")
+            return false;
+        
+        return (CheckCarry(input1, n - 1) && CheckCarryOver(input2, n - 1)) 
+               || (CheckCarry(input2, n - 1) && CheckCarryOver(input1, n -1));
+    }
+
+    private static bool CheckCarry(string wire, int n)
+    {
+        if (!Connections.TryGetValue(wire, out var value))
+            return false;
+        
+        var (input1, input2, gate) = value;
+        if (gate != "AND")
+            return false;
+
+        return SortedPair(input1, input2) == (Wire('x', n), Wire('y', n));
+    }
+
+    private static bool CheckCarryOver(string wire, int n)
+    {
+        if (!Connections.TryGetValue(wire, out var value))
+            return false;
+        
+        var (input1, input2, gate) = value;
+        if (gate != "AND")
+            return false;
+
+        return (CheckXor(input1, n) && CheckCarryBit(input2, n))
+               || (CheckXor(input2, n) && CheckCarryBit(input1, n));
+    }
+    
+    private static string Wire(char ch, int n)
+    {
+        return ch + n.ToString("00");
+    }
+
+    private static (T, T) SortedPair<T>(T item1, T item2) where T: IComparable<T>
+    {
+        return item1.CompareTo(item2) <= 0 ? (item1, item2) : (item2, item1);
+    }
+ }
