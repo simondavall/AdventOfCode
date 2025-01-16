f9681c0171b1088d7e085df7d03a133543bd9abe
Simon Da Vall <simon.davall@gmail.com>
2025-01-16 10:19:08 +0000

    Aoc2022 Day2

---
+++ 2022/02/HelperFunctions.cs
@@ -0,0 +1,108 @@
+namespace _02;
+
+internal static partial class Program
+{
+    private static List<T> List<T>(List<T> list)
+    {
+        return list;
+    }
+
+    private static IEnumerable<int> Range(int n)
+    {
+        return Enumerable.Range(0, n);
+    }
+
+    private static IEnumerable<int> Range(int start, int n)
+    {
+        return Enumerable.Range(start, n);
+    }
+
+    private static int ToInt(this string str)
+    {
+        if (int.TryParse(str, out var value))
+            return value;
+
+        throw new InvalidCastException($"Not a valid integer: {str}");
+    }
+
+    private static long ToLong(this string str)
+    {
+        if (long.TryParse(str, out var value))
+            return value;
+
+        throw new InvalidCastException($"Not a valid integer: {str}");
+    }
+
+    private static int[] ToIntArray(this string[] array)
+    {
+        var intArray = new int[array.Length];
+        for (var i = 0; i < array.Length; i++)
+        {
+            intArray[i] = array[i].ToInt();
+        }
+
+        return intArray;
+    }
+
+    private static long[] ToLongArray(this string[] array)
+    {
+        var longArray = new long[array.Length];
+        for (var i = 0; i < array.Length; i++)
+        {
+            longArray[i] = array[i].ToLong();
+        }
+
+        return longArray;
+    }
+
+    private static char[][] ToCharArray(this string[] array)
+    {
+        var charArr = new char[array.Length][];
+        for (var i = 0; i < array.Length; i++)
+            charArr[i] = array[i].ToCharArray();
+
+        return charArr;
+    }
+
+    private static (T first, T second) ToTuplePair<T>(this T[] array)
+    {
+        return array.Length switch
+        {
+            > 2 => throw new ArgumentException(
+                $" Too many array members.{array.Length} This method requires an array of length 2."),
+            < 2 => throw new ArgumentException(
+                $" Too few array members.{array.Length} This method requires an array of length 2."),
+            _ => (array[0], array[1])
+        };
+    }
+
+    private static (long first, long second, long third) ToLongTupleTriple(this string[] array)
+    {
+        return array.Length switch
+        {
+            > 3 => throw new ArgumentException(
+                $" Too many array members.{array.Length} This method requires an array of length 2."),
+            < 3 => throw new ArgumentException(
+                $" Too few array members.{array.Length} This method requires an array of length 2."),
+            _ => (array[0].ToLong(), array[1].ToLong(), array[2].ToLong())
+        };
+    }
+
+    private static List<T> ToSortedList<T>(this HashSet<T> set)
+    {
+        var list = set.ToList();
+        list.Sort();
+        return list;
+    }
+
+    private static List<T> Sorted<T>(this List<T> list)
+    {
+        list.Sort();
+        return list;
+    }
+
+    private static List<T> SortedDesc<T>(this List<T> list)
+    {
+        return list.OrderDescending().ToList();
+    }
+}
---
+++ 2022/02/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _02;
+
+internal static partial class Program
+{
+    private static readonly Dictionary<(char, char), int> Outcomes = new()
+    {
+        { ('A', 'X'), 4 },
+        { ('A', 'Y'), 8 },
+        { ('A', 'Z'), 3 },
+        { ('B', 'X'), 1 },
+        { ('B', 'Y'), 5 },
+        { ('B', 'Z'), 9 },
+        { ('C', 'X'), 7 },
+        { ('C', 'Y'), 2 },
+        { ('C', 'Z'), 6 },
+    };
+    
+    private static readonly Dictionary<(char, char), int> RevisedOutcomes = new()
+    {
+        { ('A', 'X'), 3 },
+        { ('A', 'Y'), 4 },
+        { ('A', 'Z'), 8 },
+        { ('B', 'X'), 1 },
+        { ('B', 'Y'), 5 },
+        { ('B', 'Z'), 9 },
+        { ('C', 'X'), 2 },
+        { ('C', 'Y'), 6 },
+        { ('C', 'Z'), 7 },
+    };
+    
+    internal static void Main()
+    {
+        const string filename = "input.txt";
+        var input = File.ReadAllText($"{filename}").Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+        
+
+        Console.WriteLine($"Part 1: {PartOne(input)}");
+        Console.WriteLine($"Part 2: {PartTwo(input)}");
+    }
+
+    private static long PartOne(string [] gamePlays)
+    {
+        long tally = 0;
+        foreach (var round in gamePlays)
+        {
+            tally += Outcomes[(round[0], round[2])];
+        }
+        return tally;
+    }
+
+    private static long PartTwo(string [] gamePlays)
+    {
+        long tally = 0;
+        foreach (var round in gamePlays)
+        {
+            tally += RevisedOutcomes[(round[0], round[2])];
+        }
+        return tally;
+    }
+}
