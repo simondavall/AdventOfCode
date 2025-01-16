2873ea31f79ee50dc5520242993a13d1338919d0
Simon Da Vall <simon.davall@gmail.com>
2025-01-16 22:03:38 +0000

    Aoc2022 Day6

---
+++ 2022/06/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _06;
+
+internal static class Program
+{
+    private const long ExpectedPartOne = 1093;
+    private const long ExpectedPartTwo = 3534;
+    private const string Day = "_06";
+
+    public static int Main(string[] args)
+    {
+        var filename = "input_06.txt";
+        if (args.Length > 1 && !string.IsNullOrWhiteSpace(args[1]))
+            filename = args[1];
+
+        var input = File.ReadAllText($"{filename}").Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        
+        var resultPartOne = PartOne(input[0]);
+        Console.WriteLine($"Day{Day} Part 1: {resultPartOne}");
+        var resultPartTwo = PartTwo(input[0]);
+        Console.WriteLine($"Day{Day} Part 2: {resultPartTwo}");
+
+        return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
+    }
+
+    private static long PartOne(string signalInput)
+    {
+        var signal = signalInput.ToList();
+        return FindMarkerLocation(4, signal);
+    }
+
+    private static long PartTwo(string signalInput)
+    {
+        var signal = signalInput.ToList();
+        return FindMarkerLocation(14, signal);
+    }
+
+    private static long FindMarkerLocation(int markerLength, List<char> signal)
+    {
+        var count = 0;
+        while (signal.Count >= markerLength)
+        {
+            var marker = signal.Take(markerLength);
+            if (marker.Distinct().ToArray().Length == markerLength)
+            {
+                return count + markerLength;
+            }
+            signal.RemoveAt(0);
+            count++;
+        }
+
+        return 0;
+    }
+}
